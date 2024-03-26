aabd114ce2a5419f796c89e93bd83f7a0873aaa7
Kanae132 <kana2525810@gmail.com>
2024-03-26 20:12:24 +0700

    initial commit

---
+++ Programming Theory Project/Assets/Scripts/Title Manager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+}
